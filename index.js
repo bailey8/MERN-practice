d2d46c4141d02e9e0aa3c3da1cd66f8f3c90cf7e
bailey8 <bailey8@buffalo.edu>
2020-07-21 19:16:48 -0400

    secured secret keys

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 // This is our express App. It listens for requests and routes them to different route handlers
 // All our route handlers will be registered with this App
 const app = express()
 
-app.get('/', (req,res) => {
-    res.send({hi:'there'})
-})
+// new GoogleStrategy(args) creates a new instance of the google passport strategy
+// the args specify how exactly we want to authenticate with google
+// passport.use() makes passport aware of the oauth strategy
+passport.use(new GoogleStrategy());
 
 
 
