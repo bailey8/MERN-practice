bdbc2f4fad3f6491ca3dc3c2bb1bafce7fe39747
bailey8 <bailey8@buffalo.edu>
2020-07-22 23:35:10 -0400

    Added Mongo and Mongoose basic config

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require("express");
 
+// Mongoose config
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+mongoose.connect(keys.mongoURI)
+
+
+// Will automatically get executed
+require("./services/passport");
+require('./models/User')
 
 // This is our express App. It listens for requests and routes them to different route handlers
 // All our route handlers will be registered with this App
-const app = express()
-
-// new GoogleStrategy(args) creates a new instance of the google passport strategy
-// the args specify how exactly we want to authenticate with google
-// passport.use() makes passport aware of the oauth strategy
-passport.use(new GoogleStrategy());
+const app = express();
 
 
+// --------------Dont do it this way -------------------
+// const authRoutes = require("./routes/authRoutes");
+// authRoutes(app);
+//-----------Instead do it this way -------------------------
+require('./routes/authRoutes')(app)
 
-const PORT = process.env.PORT || 5000 // Heroku sets this, and in development use 5000
-app.listen(PORT)
+
+
+
+const PORT = process.env.PORT || 5000; // Heroku sets this, and in development use 5000
+app.listen(PORT);
 // app.listen(5000) - static port binding
